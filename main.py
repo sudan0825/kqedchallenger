f45aabcda162958e09c62275e5f53eb5025895df
sudan0825 <sudan0825@gmail.com>
2016-09-18 19:24:47 -0700

    change main.py to solve server problem

--- main.py
+++ main.py
@@ -1,18 +1,24 @@
-from flask import Flask
-app = Flask(__name__)
-app.config['DEBUG'] = True
 
-# Note: We don't need to call run() since our application is embedded within
-# the App Engine WSGI application server.
+import webapp2
 
 
-@app.route('/')
-def hello():
-  """Return a friendly HTTP greeting."""
-  return 'Hello World!'
+class RestHandler(webapp2.RequestHandler):
+
+  def dispatch(self):
+    #time.sleep(1)
+    super(RestHandler, self).dispatch()
 
 
-@app.errorhandler(404)
-def page_not_found(e):
-  """Return a custom 404 error."""
-  return 'Sorry, nothing at this URL.', 404
+  def SendJson(self, r):
+    self.response.headers['content-type'] = 'text/plain'
+    self.response.write(json.dumps(r))
+
+
+
+APP = webapp2.WSGIApplication([
+
+  ('/rest/', RestHandler),
+
+], debug=True)
+
+
