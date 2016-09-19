0c3ceb607ade0e4772222eecc69c98d3217e8b93
sudan0825 <sudan0825@gmail.com>
2016-09-19 00:19:40 -0700

    add httpinterceptor

--- app/app.js
+++ app/app.js
@@ -21,6 +21,34 @@ App.config(['$locationProvider', '$routeProvider',  function($locationProvider, 
 
 
 }]);
+App.factory('myHttpInterceptor', function($rootScope, $q) {
+  return {
+    'requestError': function(config) {
+      $rootScope.status = 'HTTP REQUEST ERROR ' + config;
+      return config || $q.when(config);
+    },
+    'responseError': function(rejection) {
+      $rootScope.status = 'HTTP RESPONSE ERROR ' + rejection.status + '\n' +
+        rejection.data;
+      return $q.reject(rejection);
+    },
+  };
+});
+App.config(function($httpProvider) {
+  $httpProvider.interceptors.push('myHttpInterceptor');
+});
+
+App.factory('guestService', function($rootScope, $http, $q, $log) {
+  $rootScope.status = 'Retrieving data...';
+  var deferred = $q.defer();
+  $http.get('rest/query')
+    .success(function(data, status, headers, config) {
+      $rootScope.guests = data;
+      deferred.resolve();
+      $rootScope.status = '';
+    });
+  return deferred.promise;
+});
 
 
 
